85b3b7cea360be59aa56f6a9b67b713f9905cc41
Kristoffer Walker <kris@kixx.name>
2013-06-09 16:21:42 -0400

    define data struct for parseWidget()
     On branch master
    
    	modified:   test/save_html5_widget.js

--- test/save_html5_widget.js
+++ test/save_html5_widget.js
@@ -6,8 +6,19 @@ describe("save_html5_widget", function () {
     describe("parseWidget()", function () {
 
         it("should return a data struct", function () {
-            save_html5_widget.parseWidget();
-            (true).should.be.true;
+            var struct = save_html5_widget.parseWidget();
+
+            struct.id.should.be.a('string');
+            struct.name.should.be.a('string');
+            struct.owner.should.be.a('string');
+            struct.filename.should.be.a('string');
+            struct.height.should.be.a('number');
+            struct.width.should.be.a('number');
+            struct.mainHTML.should.be.a('string');
+
+            struct.zip.extractAllTo.should.be.a('function');
+            struct.zip.getEntries.should.be.a('function');
+            struct.zip.getEntries().should.be.an.instanceOf(Array);
             return;
         });
 
