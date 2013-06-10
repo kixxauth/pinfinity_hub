d466f63daf24174b50effef4c8c8d7e082e57f23
Kristoffer Walker <kris@kixx.name>
2013-06-09 20:34:24 -0400

    extracting zip file with tests
     On branch master
    
    	modified:   business/save_html5_widget.coffee
    	new file:   test/fixtures/widgets/google-maps.wdgt.zip
    	modified:   test/save_html5_widget.js

--- business/save_html5_widget.coffee
+++ business/save_html5_widget.coffee
@@ -1,3 +1,34 @@
-exports.parseWidget = () ->
+FS    = require 'fs'
+NPATH = require 'path'
+ZIP   = require 'adm-zip'
+
+# FIXME: These configs should be environment specific settings.
+exports.GALLERY  = '/var/pinfinity_hub/gallery'
+exports.DATA     = '/var/pinfinity_hub/data'
+exports.DWNLOADS = '/var/pinfinity_hub/downloads'
+exports.TEMP     = '/tmp/pinfinity_hub'
+
+
+exports.parseWidget = (opts) ->
+    opts or= {}
+    {widgetName, emailAddress, sourcePath, zipName, tempDirectory} = opts
     rv = Object.create(null)
+    rv.id = new Date().getTime().toString()
+    rv.name = widgetName or 'unnamed'
+    rv.owner = emailAddress or ''
+    rv.zipname = zipName or "#{rv.id}.wdgt.zip"
+    rv.tempZipPath = sourcePath
+
+    # Extract the zip file.
+    rv.tempExtractLocation = "#{exports.TEMP}/#{rv.id}"
+    zip = new ZIP(sourcePath)
+    zip.extractAllTo(rv.tempExtractLocation, yes)
+
+    rv.zipEntries = zip.getEntries().filter(filterZipFiles).map (entry) ->
+        return entry.entryName
+
     return rv
+
+filterZipFiles = (entry) ->
+    if entry.isDirectory then return false
+    else return true
--- test/save_html5_widget.js
+++ test/save_html5_widget.js
@@ -6,19 +6,48 @@ describe("save_html5_widget", function () {
     describe("parseWidget()", function () {
 
         it("should return a data struct", function () {
-            var struct = save_html5_widget.parseWidget();
+            var zipFixture = __dirname +'/fixtures/widgets/google-maps.wdgt.zip';
 
+            var struct = save_html5_widget.parseWidget({
+              widgetName: 'Boston Map Widget'
+            , emailAddress: 'wdgt@example.com'
+            , sourcePath: zipFixture
+            , zipName: 'google-maps.wdgt.zip'
+            , tempDirectory: '/tmp'
+            });
+
+            // The unique ID of the widget
             struct.id.should.be.a('string');
-            struct.name.should.be.a('string');
-            struct.owner.should.be.a('string');
-            struct.filename.should.be.a('string');
+
+            // The human given name of the widget.
+            struct.name.should.equal('Boston Map Widget');
+
+            // The owner of the widget (email address).
+            struct.owner.should.equal('wdgt@example.com');
+
+            // The zip file name of the widget.
+            struct.zipname.should.equal('google-maps.wdgt.zip');
+
+            // The temporary path where the zip file was stored after upload.
+            struct.tempZipPath.should.equal(zipFixture);
+
+            // The directory where the zip file is extracted.
+            struct.tempExtractLocation.should.be.a('string');
+
+            // A list of zip entry names.
+            struct.zipEntries.should.be.an.instanceOf(Array);
+            struct.zipEntries.length.should.equal(15);
+            struct.zipEntries.forEach(function (entry) {
+              entry.should.be.a('string');
+            });
+
+            // The height from Info.plist.
             struct.height.should.be.a('number');
+            // The width from Info.plist.
             struct.width.should.be.a('number');
+            // The declared mainHTML from Info.plist.
             struct.mainHTML.should.be.a('string');
 
-            struct.zip.extractAllTo.should.be.a('function');
-            struct.zip.getEntries.should.be.a('function');
-            struct.zip.getEntries().should.be.an.instanceOf(Array);
             return;
         });
 
